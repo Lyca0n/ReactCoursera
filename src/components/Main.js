a12d6863b7997b1c0b05fd271a30de3133e2a630
Lyca0n <julio_cesar_villalva@hotmail.com>
2018-10-27 16:25:41 -0600

    single spa pt1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Menu';
 import DishDetail from './DishDetail';
+import Contact from './Contact';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
@@ -11,8 +14,12 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
             selectedDish: null
         };
     }
@@ -22,8 +29,12 @@ class Main extends Component {
     }
 
     render() {
-        const HomePage = ()=>(
-            <Home/>
+        const HomePage = () => (
+            <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
         );
 
         return (
@@ -31,7 +42,13 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/menu' component={() => <div><Menu dishes={this.state.dishes} onClick={(dishId)=> this.onDishSelect(dishId)}   /> <DishDetail/> </div>} />
+                    <Route exact path='/menu' component={() => <div>
+                        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                        <DishDetail dish={this.state.dishes[this.state.selectedDish]} 
+                            comments={this.state.comments.filter((comment) => comment.dishId==this.state.selectedDish)[0]}
+                         /> 
+                         </div>} />
+                    <Route exact path='/contactus' component={Contact} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
