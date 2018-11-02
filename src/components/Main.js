c79f8b8fbfdf3bd315592e8f084fb5c521330099
Villalva <Julio.Villalva@autozone.com>
2018-11-02 10:04:26 -0700

    redux thunk

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Menu from './Menu';
 import DishDetail from './DishDetail';
 import Contact from './Contact';
 import About from './About';
 import Header from './Header';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 import Footer from './Footer';
 import Home from './Home';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
@@ -21,40 +21,50 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
 
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
-
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes())}      
 });
 
 
-const Main = (props) => {    
-    const HomePage = () => {
-        return (
-            <Home
-                dish={props.dishes.filter((dish) => dish.featured)[0]}
-                promotion={props.promotions.filter((promo) => promo.featured)[0]}
-                leader={props.leaders.filter((leader) => leader.featured)[0]}
-            />
-        );
+class Main extends Component{   
+
+    componentDidMount() {
+        this.props.fetchDishes();
     }
-
-    const DishWithId = ({ match }) => {
-        console.log(match);
-        return (
-            <DishDetail dish={props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                comments={props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-                addComment={props.addComment}
+      
+    render(){ 
+        const HomePage = () => {
+            return (
+                <Home 
+                dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                dishesLoading={this.props.dishes.isLoading}
+                dishesErrMess={this.props.dishes.errMess}
+                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.props.leaders.filter((leader) => leader.featured)[0]}
             />
-        );
-    };
-
-    return (
+            );
+        }
+    
+        const DishWithId = ({ match }) => {
+            console.log(match);
+            return (
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                isLoading={this.props.dishes.isLoading}
+                errMess={this.props.dishes.errMess}
+                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                addComment={this.props.addComment}
+              />
+            );
+        };
+    
+        return (
         <div>
             <Header />
             <div>
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/aboutus' component={() => <About leaders={props.leaders} />} />} />
-                  <Route exact path='/menu' component={() => <Menu dishes={props.dishes} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
                   <Redirect to="/home" />
@@ -62,8 +72,8 @@ const Main = (props) => {
             </div>
             <Footer />
         </div>
-    );
-
+        );
+    }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './Loading';
+
 
     function RenderMenuItem ({dish, onClick}) {
         return (
@@ -17,7 +19,8 @@ import { Link } from 'react-router-dom';
     }
 
     const Menu = (props) => {
-        const menu = props.dishes.map((dish) => {
+        console.log(props);
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
                     <RenderMenuItem dish={dish} onClick={props.onClick} />
@@ -25,6 +28,26 @@ import { Link } from 'react-router-dom';
             );
         });
 
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         return (
             <div className="container">
                 <div className="row">
