c0651e803042a26edbd81c5088004f757a745985
Villalva <Julio.Villalva@autozone.com>
2018-11-02 18:12:22 -0700

    react animation components

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -8,32 +8,43 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './Loading';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 function RenderDish({ dish }) {
     return (<div className="col-12 col-md-5 m-1">
-        <Card>
-            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     </div>);
 }
 
 
-function RenderComments({comments, postComment, dishId}) {
+function RenderComments({ comments, postComment, dishId }) {
     return (
         <div className="col-12 col-md-5 m-1">
             {comments.length > 0 && <h4>Comments</h4>}
             <ul className="list-unstyled">
-                {comments.map((comment, index) => (
-                    <li key={comment.id}>
-                        <p>{comment.comment} </p>
-                        <p>{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
-                    </li>
-                ))}
+                <Stagger in>
+                    {comments.map((comment, index) => (
+                        <Fade in>
+                        <li key={comment.id}>
+                            <p>{comment.comment} </p>
+                            <p>{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
+                        </li>
+                        </Fade>
+                    ))}
+                </Stagger>
             </ul>
             <CommentForm dishId={dishId} postComment={postComment} />
         </div >
@@ -42,18 +53,18 @@ function RenderComments({comments, postComment, dishId}) {
 
 export default (props) => {
     if (props.isLoading) {
-        return(
+        return (
             <div className="container">
-                <div className="row">            
+                <div className="row">
                     <Loading />
                 </div>
             </div>
         );
     }
     else if (props.errMess) {
-        return(
+        return (
             <div className="container">
-                <div className="row">            
+                <div className="row">
                     <h4>{props.errMess}</h4>
                 </div>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { Loading } from './Loading';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -18,6 +19,11 @@ function RenderCard({ item, isLoading, errMess }) {
         );
     }    
     return (
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>        
         <Card>
             <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
@@ -26,6 +32,7 @@ function RenderCard({ item, isLoading, errMess }) {
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
+        </FadeTransform>
     );
 }
 
